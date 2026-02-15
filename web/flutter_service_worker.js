b739cabf0e83a6855870a1b5cad230e4a85328c8
Swalab Tech <238155210+swalabtech@users.noreply.github.com>
2026-02-15 09:11:21 -0800

    fix: update  frontend for unified journal and timeline screen

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b9e8703c0d9e48cc51c78abc4cc4f7d2",
+const RESOURCES = {"flutter_bootstrap.js": "c7702249d4edc64f89c926e65c7caf50",
 "LICENSE.md": "7d59caa309cfe6180e3b920e57ac225c",
 "version.json": "4462094ec4e181765fc51177700378ac",
-"index.html": "48697268ee74011f7562316499f2f2d9",
-"/": "48697268ee74011f7562316499f2f2d9",
-"main.dart.js": "26d6c4f238e80554a9fd9a2b59eb96fb",
+"index.html": "282b2962f409cd57282f95cf6d4b7dfc",
+"/": "282b2962f409cd57282f95cf6d4b7dfc",
+"main.dart.js": "fc93649b0335eefbbfe4313e6d0e23ed",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "e08d2f14e458d3d49ed98d5bebedf75b",
 "icons/Icon-192.png": "1f60b4972389bd528b30f7b1c69b5ca1",
