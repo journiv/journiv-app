89328b7085f11ce05574001dc0483eaf71e1c8e4
Swalab Tech <238155210+swalabtech@users.noreply.github.com>
2026-02-22 23:45:54 -0800

    fix: spacing and layout in frontend (#456)

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1941414405"
+    serviceWorkerVersion: "2769187335"
   }
 });
